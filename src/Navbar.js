001d6b3c5c5593e41d9c8626ca9ecf247cebd4ec
hopeVaughn <hopevaughnwarren@gmail.com>
2022-11-23 11:48:20 -0800

    finish project

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,20 +1,34 @@
 import React, { useState, useRef, useEffect } from 'react'
 import { FaBars, FaTwitter } from 'react-icons/fa'
 import { links, social } from './data'
-import logo from './logo.svg'
+import logo from './logo2.svg'
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = '0px';
+    }
+  }, [showLinks])
+
+
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} className='logo' alt="navigation logo" />
-          <button className='nav-toggle'>
+          <button className='nav-toggle' onClick={() => setShowLinks(!showLinks)}>
             <FaBars />
           </button>
         </div>
-        <div className="links-container show-container">
-          <ul className='links'>
+        <div className='links-container' ref={linksContainerRef}>
+          <ul className='links' ref={linksRef}>
             {links.map((link) => {
               const { id, url, text } = link;
               return <li key={id}>
@@ -25,6 +39,7 @@ const Navbar = () => {
             })}
           </ul>
         </div>
+
         <ul className="social-icons">
           {social.map((socialIcon) => {
             const { id, url, icon } = socialIcon;
